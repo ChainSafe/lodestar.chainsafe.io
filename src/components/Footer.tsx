c469737c88d5cb34a1ce4fbfb2deb11207472b84
Patrick Michot <k0m0d0tr0n@gmail.com>
2020-06-22 14:44:34 -0500

    link styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,11 +6,11 @@ export default function(): JSX.Element {
   return (
     <div className='lodestar-footer'>
       <span className='content has-text-centered'>
-        Made with ❤️ by <a className='is-link has-text-danger is-family-code' href='https://chainsafe.io'>ChainSafe Systems</a>
+        Made with ❤️ by <a href='https://chainsafe.io'>ChainSafe Systems</a>
       </span>
       <span className='content has-text-centered is-small is-family-code'>
         <span style={{float: 'right'}}>
-          <a className='is-link has-text-grey'
+          <a
             href='https://github.com/ChainSafe/lodestar'>
             GitHub Monorepo
           </a>
