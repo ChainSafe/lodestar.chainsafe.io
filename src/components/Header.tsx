d55ae5f648ae0247c90b6820d8db8ff97d30fd45
3xtr4t3rr3str14l <k0m0d0tr0n@protonmail.com>
2021-03-04 15:21:02 -0600

    fix logo skewing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,12 @@ import fullSize from "./images/FullMark-BLACK.png";
 export default function(): JSX.Element {
   return (
     <div className='section'>
-      <span className='container lodestar-header'>
-        <img
-          className="attachment-full size-full lodestar-logo"
-          src={fullSize}
-          data-recalc-dims="1"
-          alt="Lodestar icon"
-        />
-      </span>
+      <img
+        className="attachment-full size-full lodestar-logo"
+        src={fullSize}
+        data-recalc-dims="1"
+        alt="Lodestar icon"
+      />
       <span className="heading-tagline">Utility apps made and used by <a target="_blank" rel="noopener noreferrer" href="https://chainsafe.github.io/lodestar/">Lodestar</a>, Ethereum 2.0 in Typescript.</span>
     </div>
   );
